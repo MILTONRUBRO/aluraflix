1ec29c695e8f2720a0d3ab8f0a055f1536453e24
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-07-25 15:52:51 -0300

    Adicionando logs

--- src/main/java/br/com/mosdev/aluraflix/controller/VideoController.java
+++ src/main/java/br/com/mosdev/aluraflix/controller/VideoController.java
@@ -1,6 +1,7 @@
 package br.com.mosdev.aluraflix.controller;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.validation.Valid;
 
@@ -25,20 +26,25 @@ public class VideoController {
 	@Autowired
 	private VideoService videoService;
 	
+	private static final Logger logger = Logger.getLogger( VideoController.class.getName() );
+	
 	@GetMapping("api/videos")
 	public ResponseEntity<List<VideoDTO>> listAllVideos(){
+		logger.info("Busca todos os videos");
 		List<Video> videos = videoService.findAllVideos();
 		return ResponseEntity.ok(VideoDTO.convert(videos));
 	}
 	
 	@GetMapping("api/videos/{idVideo}")
 	public ResponseEntity<VideoDTO> getVideo(@PathVariable("idVideo") Long idVideo) {
+		logger.info("Busca um video de id: " + idVideo);
 		Video video = videoService.getVideo(idVideo);
 		return ResponseEntity.ok(new VideoDTO(video));
 	}
 	
 	@PostMapping("api/videos")
 	public ResponseEntity<VideoDTO> saveVideo(@Valid @RequestBody VideoRequest request){
+		logger.info("Adicionando um novo video");
 		Video video = request.newVideo();
 		return ResponseEntity.ok(new VideoDTO(videoService.save(video)));
 	}
@@ -46,12 +52,15 @@ public class VideoController {
 	@PutMapping("api/videos/{idVideo}")
 	public ResponseEntity<VideoDTO> updateVideo(@Valid @RequestBody VideoRequest request, 
 			@PathVariable("idVideo") Long idVideo){
+		logger.info("Atualizar video de id: " + idVideo);
+
 		Video video = request.newVideo(); 
 		return ResponseEntity.ok(new VideoDTO(videoService.update(video, idVideo)));
 	}
 	
 	@DeleteMapping("api/videos/{idVideo}")
 	public ResponseEntity<Void> deleteVideo(@PathVariable("idVideo") Long idVideo) {
+		logger.info("Deletar video de id: " + idVideo);
 		videoService.delete(idVideo);
 		return ResponseEntity.ok().build();
 	}
