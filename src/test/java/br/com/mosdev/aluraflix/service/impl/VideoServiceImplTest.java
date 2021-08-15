6393b5de291f251af2d1c23af1f29de706112df2
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-08-15 12:15:00 -0300

    Adicionando testes para service de video

---
+++ src/test/java/br/com/mosdev/aluraflix/service/impl/VideoServiceImplTest.java
@@ -0,0 +1,48 @@
+package br.com.mosdev.aluraflix.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.mosdev.aluraflix.model.Video;
+import br.com.mosdev.aluraflix.repository.VideoRepository;
+import br.com.mosdev.aluraflix.service.VideoService;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class VideoServiceImplTest {
+	
+	@MockBean
+	private VideoRepository  videoRepository;
+	
+	@Autowired
+	private VideoService videoService;
+	
+	private static final String TITULO = "Alien o oitavo passageiro";
+	private static final String DESCRICAO = "Aliens, suspense e drama";
+	private static final String URL = "https://www.youtube.com/watch?v=Hdki9ZDbI4ke";
+
+
+	@Test
+	void testSaveVideo() {
+		BDDMockito.given(videoRepository.save(Mockito.any(Video.class))).willReturn(getMockVideo());
+		Video videoSaved = videoService.save(new Video());
+		
+		assertNotNull(videoSaved);
+	}
+
+	private Video getMockVideo() {
+		return new Video(TITULO, DESCRICAO, URL);
+	}
+
+}
