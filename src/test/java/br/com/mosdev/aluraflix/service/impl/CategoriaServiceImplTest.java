e00a58b0ade82fdec44857c09faac758b0a1c727
Milton Oliveira Souza <miltonrubro@gmail.com>
2021-08-19 14:59:47 -0300

    Criando teste para salvar uma categoria

---
+++ src/test/java/br/com/mosdev/aluraflix/service/impl/CategoriaServiceImplTest.java
@@ -0,0 +1,45 @@
+package br.com.mosdev.aluraflix.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.mosdev.aluraflix.model.Categoria;
+import br.com.mosdev.aluraflix.repository.CategoriaRepository;
+import br.com.mosdev.aluraflix.service.CategoriaService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class CategoriaServiceImplTest {
+	
+	@MockBean
+	private CategoriaRepository  categoriaRepository;
+	
+	@Autowired
+	private CategoriaService  categoriaService;
+	
+	private static final String TITULO = "Titulo teste";
+	private static final String COR = "azul";
+
+	@Test
+	public void testSaveCategorie() {
+		BDDMockito.given(categoriaRepository.save(Mockito.any(Categoria.class))).willReturn(getCategoria());
+		Categoria categorie = categoriaService.save(new Categoria());
+		
+		assertNotNull(categorie);
+	}
+
+	private Categoria getCategoria() {
+		return new Categoria(TITULO, COR);
+	}
+
+}
